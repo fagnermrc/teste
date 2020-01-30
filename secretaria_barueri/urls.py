5af1e9168719b0b2e348bbc6e138c62d6f939389
unknown <vinnesousa22@gmail.com>
2020-01-30 14:22:22 -0300

    Registrar o método de cadastro de vaga nas urls

--- secretaria_barueri/urls.py
+++ secretaria_barueri/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from usuarios.views import *
+from vaga.views import *
 
 urlpatterns = [
     path('cadastro/pf',cadastrar_pessoa_fisica),
     path('cadastro/pf/acessibilidade', acessibilidade_cadastro),
     path('cadastro/pj', cadastrar_empresa),
+    path('cadastro/vaga', cadastro_vaga),
     path('admin/', admin.site.urls),
 ]
